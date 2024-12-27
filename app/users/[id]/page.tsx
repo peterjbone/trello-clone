0ff35ee5c9a6d3a2d12a7bb6a4a31683e4219e86
peterjbone <peterjbone41@gmail.com>
2024-12-27 08:02:40 -0500

    practicing with params

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const IDpage = () => {
+const IDpage = ({ params }: { params: { id: string } }) => {
 	return (
 		<div>
 			<h2>IDpage</h2>
+			<p>id: {params.id}</p>
 		</div>
 	);
 };
