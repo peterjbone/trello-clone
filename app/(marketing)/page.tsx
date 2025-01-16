7c560f641b08c8599243b8d8a700772fecfc7446
peterjbone <peterjbone41@gmail.com>
2025-01-16 09:34:36 -0500

    adding structure and classes to Marketing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -18,7 +18,8 @@ const MarketingPage = () => {
 				</div>
 			</div>
 			<div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
-				Collaborate, manage new projects and reach new productivity peaks.
+				Collaborate, manage new projects and reach new productivity peaks. From high
+				rises to home office, the way your team works
 			</div>
 		</div>
 	);
