f799be2b6dacf9199e4144bc614209a153545b1e
peterjbone <peterjbone41@gmail.com>
2025-01-16 09:24:24 -0500

    adding classes to Marketing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
-	return <div>layout</div>;
+	//* COMPONENTE
+	return (
+		<div className="h-full bg-slate-100">
+			{/* Navbar here */}
+			<main className="pt-40 pb-20 bg-slate-100">{children}</main>
+		</div>
+	);
 };
 
 export default MarketingLayout;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,18 @@
+import { Medal } from "lucide-react";
 import React from "react";
 
-const Page = () => {
-	return <div>page</div>;
+const MarketingPage = () => {
+	//***************************** COMPONENTE
+	return (
+		<div className="flex items-center justify-center flex-col">
+			<div className="flex items-center justify-center flex-col">
+				<div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+					<Medal className="h-6 w-6 mr-2" />
+					No 1 Task managment
+				</div>
+			</div>
+		</div>
+	);
 };
 
-export default Page;
+export default MarketingPage;
