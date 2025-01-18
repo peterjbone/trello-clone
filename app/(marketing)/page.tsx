359edec121bbb230dfbf1bda719218e5d61f40f9
peterjbone <peterjbone41@gmail.com>
2025-01-18 12:00:17 -0500

    marketing page updated

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,12 +3,18 @@ import { Medal } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const textFont = Poppins({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500"]
+});
 
 const MarketingPage = () => {
 	//***************************** COMPONENTE
 	return (
 		<div className="flex items-center justify-center flex-col">
-			<div className="flex items-center justify-center flex-col">
+			<div className={cn("flex items-center justify-center flex-col")}>
 				<div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
 					<Medal className="h-6 w-6 mr-2" />
 					No 1 Task managment
