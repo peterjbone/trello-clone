96af91c5bc04ee1f2860116970a8391e9c396124
peterjbone <peterjbone41@gmail.com>
2025-01-18 12:51:52 -0500

    marketing page updated

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
@@ -5,7 +7,12 @@ export const Navbar = () => {
 		<div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
 				<Logo />
-				<div className="space-x-4 md:block md:w-auto flex items-center"></div>
+				<div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+					<Button size="sm" variant="outline" asChild>
+						<Link href="/sign-in">Login</Link>
+					</Button>
+					<Button>get tasktify for free</Button>
+				</div>
 			</div>
 		</div>
 	);
