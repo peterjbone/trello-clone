aad5e879e1f3b6aa7ca2f9e6abfbf876ac66f152
peterjbone <peterjbone41@gmail.com>
2025-01-18 12:06:15 -0500

    marketing page updated

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const Navbar = () => {
-	return <div>navbar</div>;
+	return (
+		<div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+			navbar
+		</div>
+	);
 };
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Navbar } from "./_components/navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
 	//* COMPONENTE
 	return (
 		<div className="h-full bg-slate-100">
 			{/* Navbar here */}
+			<Navbar />
 			<main className="pt-40 pb-20 bg-slate-100">{children}</main>
 		</div>
 	);
