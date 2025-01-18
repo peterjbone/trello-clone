39a3dd4a5603acd2b9e97e4be7c8d9115d665666
peterjbone <peterjbone41@gmail.com>
2025-01-18 12:26:02 -0500

    marketing page updated

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Navbar = () => {
 	return (
 		<div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
-			navbar
+			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between"></div>
 		</div>
 	);
 };
