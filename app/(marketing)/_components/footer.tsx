f3b74d383ebd6312a9f81e9327c926f2ed8538be
peterjbone <peterjbone41@gmail.com>
2025-01-21 10:38:38 -0500

    footer created

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -8,12 +8,8 @@ export const Navbar = () => {
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
 				<Logo />
 				<div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-					<Button size="sm" variant="outline" asChild>
-						<Link href="/sign-in">Login</Link>
-					</Button>
-					<Button size="sm" asChild>
-						<Link href="/sign-up">get tasktify for free</Link>
-					</Button>
+					<Button size="sm">Privacy policy</Button>
+					<Button size="sm">Terms of service</Button>
 				</div>
 			</div>
 		</div>
