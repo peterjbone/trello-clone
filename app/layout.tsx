17ffbe43fc5372ca9244cfd8f4eeca9aff43105b
peterjbone <peterjbone41@gmail.com>
2025-01-21 10:55:16 -0500

    editing the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,14 @@ export const metadata: Metadata = {
 	title: {
 		default: siteConfig.name,
 		template: `%s | ${siteConfig.name}`
-	}
+	},
+	description: siteConfig.description,
+	icons: [
+		{
+			url: "/logo.svg",
+			href: "/logo.svg"
+		}
+	]
 };
 
 export default function RootLayout({
