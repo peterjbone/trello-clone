387ee64ab815de743b2083a9d9c845cca10bff05
peterjbone <peterjbone41@gmail.com>
2025-01-22 21:10:05 -0500

    editing the layout of dashboard

--- app/(plataform)/(dashboard)/organization/layout.tsx
+++ app/(plataform)/(dashboard)/organization/layout.tsx
@@ -2,7 +2,10 @@ const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
 	//*************************** COMPONENTE
 	return (
 		<main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
-			<div>{children}</div>
+			<div className="flex gap-x-7">
+				<div className="w-64 shrink-0 hidden md:block">{/* sidebar */}</div>
+				{children}
+			</div>
 		</main>
 	);
 };
