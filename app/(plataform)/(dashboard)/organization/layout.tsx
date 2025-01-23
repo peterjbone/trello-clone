efeffb96be1500e4ba7dc995378b4db9f73fdaf7
peterjbone <peterjbone41@gmail.com>
2025-01-22 21:05:11 -0500

    editing the layout of dashboard

--- app/(plataform)/(dashboard)/organization/layout.tsx
+++ app/(plataform)/(dashboard)/organization/layout.tsx
@@ -1,6 +1,10 @@
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
 	//*************************** COMPONENTE
-	return <main className="pt-20">{children}</main>;
+	return (
+		<main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+			<div>{children}</div>
+		</main>
+	);
 };
 
 export default OrganizationLayout;
